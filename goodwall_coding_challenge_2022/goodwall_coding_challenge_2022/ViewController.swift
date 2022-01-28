2af1b5be56e44382e78bb4bccb4a1af16a857775
TayseerAnwar <>
2022-01-28 13:20:19 +0200

    initCommit

---
+++ goodwall_coding_challenge_2022/goodwall_coding_challenge_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goodwall_coding_challenge_2022
+//
+//  Created by Tayseer Anwar on 28/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
